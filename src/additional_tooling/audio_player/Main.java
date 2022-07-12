8708f3a041a275eea22657030375e1499fd46e4c
aryansajith <aryan.sajith04@gmail.com>
2022-07-12 12:10:29 -0400

    GUI and Music Player

---
+++ src/additional_tooling/audio_player/Main.java
@@ -0,0 +1,44 @@
+package additional_tooling.audio_player;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        // This part deals with user input for the music player
+        Scanner scanner = new Scanner(System.in);
+        String response = "";
+        // We create an AudioInputStream object to get audio input from the music file
+        File file = new File("src/additional_tooling/audio_player/Country_Roads.wav");
+        AudioInputStream audioStream =  AudioSystem.getAudioInputStream(file);
+
+        // We create a Clip object to read the audioStream object we created earlier
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        //This part handles user responses!
+        System.out.println("Today's music is Country Roads by John Denver ");
+        while(!response.equalsIgnoreCase("q")) {
+            System.out.println("p = play, s = stop, r = reset, q = quit");
+            System.out.print("Enter your choice: ");
+            response = scanner.next();
+            switch (response.toLowerCase()) {
+                case ("p") -> clip.start();
+                case ("s") -> clip.stop();
+                case ("r") -> {
+                    clip.setMicrosecondPosition(0);
+                    clip.start();
+                }
+                case ("q") -> {
+                    clip.close();
+                    scanner.close();
+                }
+                default -> System.out.println("Not a valid response ;(");
+            }
+        }
+        System.out.println("Thank you for listening!");
+    }
+
+}
