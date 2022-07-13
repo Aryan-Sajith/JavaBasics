3658634646a300aba9bb8a7f28b278d0724d7b71
aryansajith <aryan.sajith04@gmail.com>
2022-07-13 14:52:46 -0400

    A few classes to explore simple JButtons in java!

---
+++ src/additional_tooling/gui/JButton/Main.java
@@ -0,0 +1,9 @@
+package additional_tooling.gui.JButton;
+
+public class Main {
+    // JButton = A GUI button that performs an action when clicked on it
+
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+}
