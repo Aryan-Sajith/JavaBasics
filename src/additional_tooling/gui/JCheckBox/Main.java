20f7d3a4652d0001fb2489d3f87adea8e1bf4002
aryansajith <aryan.sajith04@gmail.com>
2022-07-17 15:05:35 -0400

    A few Java classes to explore JCheckBoxes in Java!

---
+++ src/additional_tooling/gui/JCheckBox/Main.java
@@ -0,0 +1,9 @@
+package additional_tooling.gui.JCheckBox;
+
+// JCheckBox = A GUI box that can be selected or deselected
+
+public class Main {
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+}
